56b7777269a779ec85ab03db329fa0afc4611b0b
Dairon Medina Caro <dairon.medina@gmail.com>
2020-10-11 22:29:39 -0400

    Bootstrap project

---
+++ lib/exsort.ex
@@ -0,0 +1,18 @@
+defmodule Exsort do
+  @moduledoc """
+  Documentation for `Exsort`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Exsort.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/exsort_test.exs
@@ -0,0 +1,8 @@
+defmodule ExsortTest do
+  use ExUnit.Case
+  doctest Exsort
+
+  test "greets the world" do
+    assert Exsort.hello() == :world
+  end
+end
